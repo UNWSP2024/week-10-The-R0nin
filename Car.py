5c4d29a96dd0072061f865bd1288b6f4d5890bcd
The-R0nin <2025esteele@isd2144.org>
2024-11-07 13:40:04 -0600

    Update Car.py
    Ethan Steele
    11/7
    Ok?

--- Car.py
+++ Car.py
@@ -19,8 +19,9 @@
 #After each call to the accelerate method, get the current speed of the car and display it.  
 #The call the brake method.  After each call 
 # to the brake method, get the current speed of the car and display it.
-speed_up = 0
+
 class Car:
+    loop = 0 
     def __init__(self, year_model, make, speed):
         self.__year_model = year_model
         self.__make =  make
@@ -45,18 +46,18 @@ class Car:
         return self.__speed
     
 #acceleration set up
-    while speed_up > 5:
-        def __init_subclass__(self, acceleration, brake) -> None:
-            self._acceleration = acceleration
-            self._brake = brake
+    def __init_subclass__(self, acceleration, brake):
+        self._acceleration = acceleration      
+        self._brake = brake
+        Car.loop += 1 
 
-        def set_acceleration(self, speed):
-            self.speed =  speed + 5 
+    def set_acceleration(self, speed):
+        self.speed =  speed + 5 
 
-        def get_acceleration(self, speed):
-            print('Current seep is:',self.__speed)
-            return self._acceleration
-        speed_up = speed_up + 1
+    def get_acceleration(self, speed):
+        print('Current seep is:',self.__speed)
+        return self._acceleration
+
 
     def set_brake(self, speed):
         self.speed = speed - 5
